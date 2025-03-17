7af4e31f049c08e48a449271e2a6c344df9fcbc1
Puchungu <nelsonalvarenga1000@gmail.com>
2025-03-17 11:20:49 -0600

    Desarrollo del ejercicio 4

--- back.js
+++ back.js
@@ -1,16 +1,15 @@
 const sendInfoGet = () =>{
+    let num = document.getElementById("num1").value;
+    let cadena = `num=${num}`;
     let xml = new XMLHttpRequest();
     xml.onreadystatechange = () => {
         if(xml.readyState == 4 && xml.status == 200){
             console.log(xml.responseText);
             document.getElementById("res").innerText = "";
             document.getElementById("res").innerHTML = xml.responseText;
-        }else {
-            console.log('Error: ' + xml.statusText);
         }
     };
-    xml.open("GET", "back.php?", true);
+    xml.open("GET", "back.php?" + cadena, true);
     xml.send();
 };
-
-sendInfoGet();
+document.getElementById("btn").addEventListener("click", sendInfoGet);
--- back.php
+++ back.php
@@ -1,6 +1,13 @@
-<?php 
-$ciudades = ["San Salvador", "Santa Ana", "San Miguel", "La Libertad", "Sonsonate"];
-foreach ($ciudades as $ciudad) {
-    echo "$ciudad <br>";
+<?php
+$num = $_GET['num'];
+
+function factorial($num) {
+    if ($num == 0) {
+        return 1;
+    } else {
+        return $num * factorial($num - 1);
+    }
 }
+
+echo 'El factorial de ' . $num . ' es: ' . factorial($num);
 ?>
