9f19503a3ada5754370ca5e50fbc0550479612bb
Puchungu <nelsonalvarenga1000@gmail.com>
2025-03-16 20:38:45 -0600

    Desarrollo del Ejercicio 3

--- back.php
+++ back.php
@@ -1,3 +1,6 @@
 <?php 
-echo "Hello World. from a AJAX request";
+$ciudades = ["San Salvador", "Santa Ana", "San Miguel", "La Libertad", "Sonsonate"];
+foreach ($ciudades as $ciudad) {
+    echo "$ciudad <br>";
+}
 ?>
